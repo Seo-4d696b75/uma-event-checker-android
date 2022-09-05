a1510c63eda6d1a768a1e2bb9250055e8e012e4a
k_senda <k_senda@yumemi.co.jp>
2022-09-06 02:07:10 +0900

    修正：DebugDialog表示項目

--- app/src/main/java/jp/seo/uma/eventchecker/ui/DebugDialogViewModel.kt
+++ app/src/main/java/jp/seo/uma/eventchecker/ui/DebugDialogViewModel.kt
@@ -4,15 +4,18 @@ import android.graphics.Bitmap
 import androidx.annotation.MainThread
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.seo.uma.eventchecker.core.DataRepository
 import jp.seo.uma.eventchecker.core.SettingRepository
 import jp.seo.uma.eventchecker.img.EventType
 import jp.seo.uma.eventchecker.img.ImageProcess
+import jp.seo.uma.eventchecker.model.GameEvent
 import javax.inject.Inject
 
 @HiltViewModel
 class DebugDialogViewModel @Inject constructor(
     private val imgProcess: ImageProcess,
     private val setting: SettingRepository,
+    private val dataRepository: DataRepository,
 ) : ViewModel() {
 
     @MainThread
@@ -21,12 +24,14 @@ class DebugDialogViewModel @Inject constructor(
         _ocrText = imgProcess.title.value
         _textImg = imgProcess.textImage.value
         _eventType = imgProcess.currentEventType.value
+        _currentEvent = dataRepository.currentEvent.value
     }
 
     private var _isGameScreen: Boolean = false
     private var _ocrText: String? = null
     private var _textImg: Bitmap? = null
     private var _eventType: EventType? = null
+    private var _currentEvent: GameEvent? = null
 
     val isGameScreen
         get() = _isGameScreen
@@ -39,6 +44,9 @@ class DebugDialogViewModel @Inject constructor(
 
     val eventType
         get() = _eventType
+
+    val currentEvent
+        get() = _currentEvent
 
     fun onDismiss() {
         setting.isDebugDialogShown = false
