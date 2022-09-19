42c763c5efe4df25dd6ff62a147a28571c130fad
k_senda <k_senda@yumemi.co.jp>
2022-09-19 12:04:30 +0900

    追加：Navigation Lib

--- app/src/main/java/jp/seo/uma/eventchecker/ui/MainViewModel.kt
+++ app/src/main/java/jp/seo/uma/eventchecker/ui/MainViewModel.kt
@@ -1,27 +1,18 @@
 package jp.seo.uma.eventchecker.ui
 
-import android.media.Image
 import android.media.projection.MediaProjection
-import android.os.SystemClock
-import android.util.Log
 import android.view.WindowManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.seo.uma.eventchecker.img.ImageProcess
-import jp.seo.uma.eventchecker.repository.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
+import jp.seo.uma.eventchecker.repository.AppRepository
+import jp.seo.uma.eventchecker.repository.DataRepository
+import jp.seo.uma.eventchecker.repository.ScreenCapture
+import jp.seo.uma.eventchecker.repository.SettingRepository
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
-/**
- * @author Seo-4d696b75
- * @version 2021/07/03.
- */
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val appRepository: AppRepository,
@@ -29,22 +20,9 @@ class MainViewModel @Inject constructor(
     private val imgProcess: ImageProcess,
     private val settingRepository: SettingRepository,
     private val capture: ScreenCapture,
-    private val searchRepository: SearchRepository,
 ) : ViewModel() {
 
-    val loading = combine(
-        imgProcess.hasInitialized,
-        dataRepository.initialized,
-    ) { v1, v2 -> !v1 || !v2 }
-        .stateIn(viewModelScope, SharingStarted.Eagerly, true)
-
     val event = appRepository.event
-
-    // observe setting values
-    private val minUpdateInterval =
-        settingRepository.minUpdateInterval.stateIn(viewModelScope, SharingStarted.Lazily)
-    private val ocrThreshold =
-        settingRepository.ocrThreshold.stateIn(viewModelScope, SharingStarted.Lazily)
 
     /**
      * 新しいデータを確認する
@@ -63,7 +41,7 @@ class MainViewModel @Inject constructor(
     }
 
     /**
-     * Gets data from disk and init
+     * ストレージからデータ読み出す
      */
     fun loadData() = viewModelScope.launch {
         try {
@@ -74,42 +52,10 @@ class MainViewModel @Inject constructor(
         }
     }
 
-    fun setMetrics(manager: WindowManager) = settingRepository.setMetrics(manager)
-
-    /**
-     * Note: this func is supposed to be called from a callback set at ImageReader,
-     * which is running NOT on main thread
-     */
-    fun updateScreen(img: Image) = runBlocking {
-        /*
-        ImageReader provides image data via a callback, whereas
-        this ViewModel uses coroutine.
-        runBlocking is used in order to call suspending style functions in blocking style
-         */
-        val start = SystemClock.uptimeMillis()
-        searchRepository.searchForEvent(img, ocrThreshold.value)
-        val now = SystemClock.uptimeMillis()
-        val minInterval = minUpdateInterval.value
-        val wait = start + minInterval - now
-        if (wait > 0L) {
-            Log.d(
-                "ViewModel",
-                "update -> wait $wait ms (min-interval $minInterval ms)"
-            )
-            delay(wait)
-        } else {
-            Log.d("ViewModel", "update -> time ${now - start} ms")
-        }
-    }
-
     fun startCapture(projection: MediaProjection) = capture.start(projection)
 
     fun stopCapture() = capture.stop()
 
-    val runningCapture = capture.running
-
-    fun setScreenCallback(callback: ((Image) -> Unit)) {
-        capture.callback = callback
-    }
+    fun setMetrics(manager: WindowManager) = settingRepository.setMetrics(manager)
 
 }
